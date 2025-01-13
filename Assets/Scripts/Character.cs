6ba18caa107a0641117d0a4cfa76b22cdbaf0db7
jub27 <55947009+jub27@users.noreply.github.com>
2025-01-14 01:30:02 +0900

    프로젝트 생성

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector2 moveDir = Vector2.zero;
+    [SerializeField]private Transform followCamTransform;
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        moveDir = inputValue.Get<Vector2>();
+    }
+
+    private void Update() {
+        Vector3 forward = followCamTransform.forward;
+        forward.y = 0;
+        Vector3 right = followCamTransform.right;
+        right.y = 0;
+
+        transform.Translate((forward * moveDir.y + right * moveDir.x) * 5 * Time.deltaTime);
+    }
+}
