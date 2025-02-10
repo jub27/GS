2c0f954639e137fafb710b2acc2e345ed472082c
jub27 <55947009+jub27@users.noreply.github.com>
2025-02-11 00:18:59 +0900

    foot ik 다시 구현

--- Assets/Scripts/FootIK.cs
+++ Assets/Scripts/FootIK.cs
@@ -4,7 +4,8 @@ public class FootIK : MonoBehaviour
 {
     private Animator animator;
 
-    private float rayDistance = 0.7f;
+    [SerializeField,Range(0, 1.0f)]
+    private float rayDistance;
 
     private void Awake() {
         animator = GetComponent<Animator>();
@@ -24,15 +25,16 @@ public class FootIK : MonoBehaviour
         animator.SetIKPositionWeight(avatarIKGoal, 1);
         animator.SetIKRotationWeight(avatarIKGoal, 1);
 
-        Ray ray = new Ray(animator.GetIKPosition(avatarIKGoal) + new Vector3(0, rayDistance / 2f, 0), Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit, rayDistance, 1 << LayerMask.NameToLayer("Ground")))
+        Ray ray = new Ray(animator.GetIKPosition(avatarIKGoal) + Vector3.up, Vector3.down);
+        Debug.DrawRay(ray.origin, Vector3.down * (rayDistance + 1), Color.red);
+        if (Physics.Raycast(ray, out RaycastHit hit, rayDistance + 1, 1 << LayerMask.NameToLayer("Ground")))
         {
             // 걸을 수 있는 땅이라면
             if (hit.transform.CompareTag("Ground"))
             {
-                Debug.Log("@@");
-                Vector3 footPosition = animator.GetIKPosition(avatarIKGoal);
-                footPosition.y = Mathf.Lerp(footPosition.y, hit.point.y + 0.1f, 0.3f);
+                Debug.Log($"{avatarIKGoal} On Ground");
+                Vector3 footPosition = hit.point;
+                footPosition.y += rayDistance;
 
                 animator.SetIKPosition(avatarIKGoal, footPosition);
                 animator.SetIKRotation(avatarIKGoal, Quaternion.LookRotation(transform.forward, hit.normal));
